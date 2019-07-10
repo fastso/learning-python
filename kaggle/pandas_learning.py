143016305130536f4add649c987b7b06c10a4acc
lihui <fastso.biko@gmail.com>
2019-07-10 14:13:01 +0900

    commit

---
+++ kaggle/pandas_learning.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+pd.DataFrame({'column A': [1, 2], 'column B': [3, 4]}, index=['Row 1', 'Row 2'])
+
+pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
