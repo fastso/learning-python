43f7ff888ca06f736a13049eac1bd938c87d9470
lihui <fastso.biko@gmail.com>
2019-07-31 15:49:43 +0900

    commit

--- atcoder/contest/abcxxx_a.py
+++ atcoder/contest/abcxxx_a.py
@@ -10,3 +10,10 @@ bytes_result = str_sample.encode('utf-8')
 # bytes to str
 bytes_sample = b'\xe3\x81\x82\xe3\x81\x84\xe3\x81\x86'
 str_result = bytes_sample.decode('utf-8')
+
+flavor_list = ['vanilla', 'chocolate', 'pecan', 'strawberry']
+for i, flavor in enumerate(flavor_list, start=1):
+    print('%d: %s' % (i, flavor))
+
+for i, flavor in enumerate(flavor_list):
+    print('%d: %s' % (i, flavor))
