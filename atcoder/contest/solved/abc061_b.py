9c5b993e1c8a4cf716f317a9ab63db559c363a17
lihui <fastso.biko@gmail.com>
2019-08-22 19:30:38 +0900

    commit

---
+++ atcoder/contest/solved/abc061_b.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+a = [0] * n
+for i in range(m):
+    x, y = map(int, input().split())
+    a[x - 1] += 1
+    a[y - 1] += 1
+
+for i in range(n):
+    print(a[i])
