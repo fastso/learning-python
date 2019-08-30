94341b75bd562e7f4aad6f3bdcba91dae5986403
lihui <fastso.biko@gmail.com>
2019-08-30 14:14:44 +0900

    commit

---
+++ atcoder/contest/solved/abc047_b.py
@@ -0,0 +1,22 @@
+w, h, n = map(int, input().split())
+
+x1 = 0
+y1 = 0
+x2 = w
+y2 = h
+
+for i in range(n):
+    x, y, a = map(int, input().split())
+    if a == 1:
+        x1 = max(x1, x)
+    elif a == 2:
+        x2 = min(x2, x)
+    elif a == 3:
+        y1 = max(y1, y)
+    elif a == 4:
+        y2 = min(y2, y)
+
+if x1 < x2 and y1 < y2:
+    print((x2 - x1) * (y2 - y1))
+else:
+    print(0)
