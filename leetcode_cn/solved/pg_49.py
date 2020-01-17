f21c8684309432a4c89117556acda34fb48dca94
lihui <fastso.biko@gmail.com>
2020-01-17 11:48:10 +0900

    commit

---
+++ leetcode_cn/solved/pg_49.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return list(ans.values())
