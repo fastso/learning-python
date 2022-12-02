8cedaa3fb933118b675793b9e71d2ae0df1ebd3c
fastso <fastso.biko@gmail.com>
2022-12-02 19:39:27 +0900

    commit

--- leetcode_cn/solved/pg_66.py
+++ leetcode_cn/solved/pg_66.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         plus = 1
-        for i in reversed(range(len(digits))):
+        for i in range(len(digits) - 1, -1, -1):
             if digits[i] == 9:
                 digits[i] = 0
                 if i == 0:
